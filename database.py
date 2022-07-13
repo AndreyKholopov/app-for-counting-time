60bc58bfcd9cb716a47258b13d003c5fad16fd45
Home Andrey <Andrey.Kholopov.Work@gmail.com>
2022-07-13 17:25:30 +0300

    finish day 2

--- database.py
+++ database.py
@@ -2,23 +2,30 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.con = sqlite3.connect('data.db')
+        self.con = sqlite3.connect('data.db', check_same_thread=False)
         self.cursor = self.con.cursor()
         self.create_task_table()
 
     def create_task_table(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS tasks(id integer PRIMARY KEY AUTOINCREMENT, task varchar(50) NOT NULL, time varchar(50))")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS tasks(id integer PRIMARY KEY AUTOINCREMENT, task varchar(50) NOT NULL, minutes varchar(2), hours varchar(50))")
         self.con.commit()
 
-    def create_task(self, task, time):
-        self.cursor.execute("INSERT INTO tasks(task, time) VALUES(?, ?)", (task, time,))
+    def create_task(self, task, minutes, hours):
+        self.cursor.execute("INSERT INTO tasks(task, minutes, hours) VALUES(?, ?, ?)", (task, minutes, hours,))
         self.con.commit()
 
-        created_task = self.cursor.execute("SELECT id, task, time FROM tasks WHERE task = ?", (task,)).fetchall()
+        created_task = self.cursor.execute("SELECT id, task, minutes, hours FROM tasks WHERE task = ?", (task,)).fetchall()
+        return created_task[-1]
+        
+    def change_task(self, taskid, task, minutes, hours):
+        self.cursor.execute("UPDATE tasks SET task = ?, minutes = ?, hours = ? WHERE id = ?", (task, minutes, hours, taskid,))
+        self.con.commit()
+
+        created_task = self.cursor.execute("SELECT id, task, minutes, hours FROM tasks WHERE task = ?", (task,)).fetchall()
         return created_task[-1]
 
     def get_tasks(self):
-        tasks = self.cursor.execute("SELECT id, task, time FROM tasks").fetchall()
+        tasks = self.cursor.execute("SELECT id, task, minutes, hours FROM tasks").fetchall()
         return tasks
 
     def delete_task(self, taskid):
--- main.py
+++ main.py
@@ -4,46 +4,63 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import TwoLineAvatarIconListItem
 
+from time import sleep
+from threading import Thread
+
 from database import Database
 
 db = Database()
 
 class MainApp(MDApp):
-    task_list_dialog = None
+    task_created_list_dialog = None
+    task_changed_list_dialog = None
 
     def build(self):
         self.theme_cls.primary_palette = "DeepPurple"
 
     def show_task_dialog(self):
-        if not self.task_list_dialog:
-            self.task_list_dialog = MDDialog(
+        if not self.task_created_list_dialog:
+            self.task_created_list_dialog = MDDialog(
                 title="Create Task",
                 type="custom",
                 content_cls=DialogContent(),
             )
 
-        self.task_list_dialog.open()
+        self.task_created_list_dialog.open()
 
     def show_change_task_dialog(self, the_list_item):
-        if not self.task_list_dialog:
-            self.task_list_dialog = MDDialog(
+        if not self.task_changed_list_dialog:
+            self.task_changed_list_dialog = MDDialog(
                 title="Change Task",
                 type="custom",
-                content_cls=DialogChangeContent(the_list_item),
+                content_cls=DialogChangeContent(),
             )
+  
+        [hours, minutes] = the_list_item.secondary_text.split(':')
+        self.task_changed_list_dialog.content_cls.ids.the_list_item = the_list_item
+        self.task_changed_list_dialog.content_cls.ids.task_text.text = the_list_item.text
+        self.task_changed_list_dialog.content_cls.ids.task_hours.text = hours
+        self.task_changed_list_dialog.content_cls.ids.task_minutes.text = minutes
 
-        self.task_list_dialog.open()
+        self.task_changed_list_dialog.open()
 
     def close_dialog(self, *args):
-        self.task_list_dialog.dismiss()
+        self.task_created_list_dialog.dismiss()
+
+    def close_change_dialog(self, *args):
+        self.task_changed_list_dialog.dismiss()
         
     def on_start(self):
+        def takeFirst(elem):
+            return elem[0]
+
         try:
             tasks = db.get_tasks()
+            tasks.sort(key=takeFirst, reverse=True)
 
             if tasks != []:
                 for task in tasks:
-                    add_task = ListItem(pk=task[0], text=task[1], secondary_text=task[2])
+                    add_task = ListItem(pk=task[0], text=task[1], secondary_text=task[3]+':'+task[2])
                     self.root.ids.container.add_widget(add_task)
 
         except Exception as e:
@@ -51,10 +68,21 @@ class MainApp(MDApp):
             pass
 
     def add_task(self, task):
-        created_task = db.create_task(task.text, 0)
+        created_task = db.create_task(task.text, 0, 0)
 
-        self.root.ids['container'].add_widget(ListItem(pk=created_task[0], text=created_task[1], secondary_text=created_task[2]))
+        self.root.ids.container.add_widget(ListItem(pk=created_task[0], text=created_task[1], secondary_text=created_task[3]+':'+created_task[2]), index=len(self.root.ids.container.children))
+
         task.text = ''
+
+    def change_task(self, the_list_item, task_text, task_minutes, task_hours, *args):
+        created_task = db.change_task(the_list_item.pk, task_text, task_minutes, task_hours)
+
+        the_list_item.text=created_task[1]
+        the_list_item.secondary_text=created_task[3]+':'+created_task[2]
+
+        task_text = ''
+        task_minutes = ''
+        task_hours = ''
 
 
 class DialogContent(MDBoxLayout):
@@ -63,13 +91,13 @@ class DialogContent(MDBoxLayout):
 
 
 class DialogChangeContent(MDBoxLayout):
-    def __init__(self, the_list_item, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        print(self)
-        self.task_text = the_list_item.text
 
 
 class ListItem(TwoLineAvatarIconListItem):
+    timer = None
+
     def __init__(self, pk=None, **kwargs):
         super().__init__(**kwargs)
         self.pk = pk
@@ -78,8 +106,52 @@ class ListItem(TwoLineAvatarIconListItem):
         self.parent.remove_widget(the_list_item)
         db.delete_task(the_list_item.pk)
 
-    def play(self, the_list_item):
-        self.IconLeftWidget.icon = "pause" if self.IconLeftWidget.icon == "play" else "play"
+    def play_or_pause(self, the_list_item, icon):
+        if not self.timer or not self.timer.is_time_going:
+            [hours, minutes] = the_list_item.secondary_text.split(':')
+            self.timer = Timer(the_list_item, hours, minutes)
+            th = Thread(target=Timer.start_timer, args=(self.timer, ))
+            th.start()
+
+            icon.icon = "pause"
+            icon.text_color = [1, 0, 0, 1]
+        else:
+            [hours, minutes] = Timer.stop_timer(self.timer)
+
+            icon.icon = "play"
+            icon.text_color = [.5, .8, 0, 1]
+
+            app.change_task(the_list_item, self.text, minutes, hours)
+
+
+class Timer():
+    is_time_going = 0
+    change_task = None
+
+    def __init__(self, the_list_item, hours, minutes, **kwargs):
+        super().__init__(**kwargs)
+        self.the_list_item = the_list_item
+        self.hours = int(hours)
+        self.minutes = int(minutes)
+        self.seconds = 0
+
+    def start_timer(self):
+        self.is_time_going = 1
+        while self.is_time_going:
+            sleep(1)
+            self.seconds += 1
+            if self.seconds == 60:
+                self.minutes += 1
+                self.seconds = 0
+                self.the_list_item.secondary_text=str(self.hours)+':'+str(self.minutes)
+                if self.minutes == 60:
+                    self.hours += 1
+                    self.minutes = 0
+                    self.the_list_item.secondary_text=str(self.hours)+':'+str(self.minutes)
+
+    def stop_timer(self):
+        self.is_time_going = 0
+        return [self.hours, self.minutes]
 
 
 if __name__ == '__main__':
